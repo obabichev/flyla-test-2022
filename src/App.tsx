e3613e1106438955d27da765863669bd349e822b
obabichev <babichev.oleg.n@gmail.com>
2022-04-09 20:00:29 +0200

    Lesson 1: typings for categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {mockCategories} from "./mock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const a: string = 'qwe';
+    return (
+        <div className="App">
+            {mockCategories.trivia_categories.map(category => (
+                <div key={category.id}>
+                    {category.name}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface CategoryResponse {
+    trivia_categories: TriviaCategory[];
+}
+
+export interface TriviaCategory {
+    id: number;
+    name: string;
+}
+
